b904ea8481ffab79993172e88a789f988c7fce73
Hanifc56 <abuhanif007sd@gmail.com>
2024-01-26 06:09:35 +0600

    data created

--- index.js
+++ index.js
@@ -26,12 +26,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // const productCollection = client.db("productDB").collection("products");
-    app.post("/products"),
-      async (req, res) => {
-        const newProduct = req.body;
-        console.log(newProduct);
-      };
+    const productCollection = client.db("productDB").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -39,7 +47,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
